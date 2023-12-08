55e4b48084084515118ee6830d650038fa8b2b63
Aditya <60684641+0x0elliot@users.noreply.github.com>
2023-12-09 04:50:24 +0530

    montor

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,7 +19,4 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("authentication/", include("authentication.urls")),
-    path("organizations/", include("authentication.organizations.urls")),
-    path("monitoring/", include("api_app.monitoring.urls")),
-    path("smartcontract/", include("api_app.smartcontract.urls")),
 ]
