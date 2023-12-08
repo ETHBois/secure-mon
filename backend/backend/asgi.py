4dd1264b432b053b66c278ffa3130a781ad79dac
Aditya <60684641+0x0elliot@users.noreply.github.com>
2023-12-08 21:23:22 +0530

    init: .gitignore

---
+++ backend/backend/asgi.py
@@ -0,0 +1,28 @@
+"""
+ASGI config for backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+import django
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+django.setup()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from api_app.monitoring.routing import websocket_urlpatterns
+
+application = application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+    }
+)
